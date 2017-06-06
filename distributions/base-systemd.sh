957b99f5f4f53bb12655de7b66a8e71712f57c55
Gruve_p <gruve-p@users.noreply.github.com>
2017-06-06 11:17:13 +0200

    Update base-systemd.sh

--- distributions/base-systemd.sh
+++ distributions/base-systemd.sh
@@ -2,13 +2,13 @@ function install_init {
 	if [ ! -d /etc/systemd/system ]; then
 		_error "/etc/systemd/system does not exist. Is systemd installed?" 8
 	fi
-	cp /tmp/electrumx/contrib/systemd/electrumx.service /etc/systemd/system/electrumx.service
-	cp /tmp/electrumx/contrib/systemd/electrumx.conf /etc/
+	cp /tmp/electrumx-grs/contrib/systemd/electrumx-grs.service /etc/systemd/system/electrumx-grs.service
+	cp /tmp/electrumx-grs/contrib/systemd/electrumx-grs.conf /etc/
 	if [ $USE_ROCKSDB == 1 ]; then
-		echo -e "\nDB_ENGINE=rocksdb" >> /etc/electrumx.conf
+		echo -e "\nDB_ENGINE=rocksdb" >> /etc/electrumx-grs.conf
 	fi
 	systemctl daemon-reload
-	systemctl enable electrumx
-	systemctl status electrumx
-	_info "Use service electrumx start to start electrumx once it's configured"
+	systemctl enable electrumx-grs
+	systemctl status electrumx-grs
+	_info "Use service electrumx-grs start to start electrumx-grs once it's configured"
 }

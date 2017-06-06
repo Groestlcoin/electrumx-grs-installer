ee272dc5be952a175520b0c928898bab9468f5e3
Gruve_p <gruve-p@users.noreply.github.com>
2017-06-06 11:15:53 +0200

    Update test_with_docker.sh

--- test/test_with_docker.sh
+++ test/test_with_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "docker run -v $(pwd)/..:/root/electrumx-grs-installer $IMAGE /root/electrumx-grs-installer/test/test.sh"
-docker run -v $(pwd)/..:/root/electrumx-grs-installer $IMAGE /root/electrumx-grs-installer/test/test.sh 2>&1 | tee /root/$$.log
-if grep -q "ElectrumX-GRS server starting" /root/$$.log; then
+echo "docker run -v $(pwd)/..:/tmp/electrumx-grs-installer $IMAGE /tmp/electrumx-grs-installer/test/test.sh"
+docker run -v $(pwd)/..:/tmp/electrumx-grs-installer $IMAGE /tmp/electrumx-grs-installer/test/test.sh 2>&1 | tee /tmp/$$.log
+if grep -q "ElectrumX-GRS server starting" /tmp/$$.log; then
     echo "Success"
     exit 0
 fi

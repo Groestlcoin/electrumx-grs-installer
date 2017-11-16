eec6f527b26f62ef392ddf67f018592aaebc9877
Gruve_p <gruve-p@users.noreply.github.com>
2017-11-16 23:05:09 +0100

    Groestlize

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-if [ -d ~/.electrumx-installer ]; then
-    echo "~/.electrumx-installer already exists."
-    echo "Either delete the directory or run ~/.electrumx-installer/install.sh directly."
+if [ -d ~/.electrumx-grs-installer ]; then
+    echo "~/.electrumx-grs-installer already exists."
+    echo "Either delete the directory or run ~/.electrumx-grs-installer/install.sh directly."
     exit 1
 fi
 if which git > /dev/null 2>&1; then
-    git clone https://github.com/bauerj/electrumx-installer ~/.electrumx-installer
+    git clone https://github.com/Groestlcoin/electrumx-grs-installer ~/.electrumx-grs-installer
 else
     which wget > /dev/null 2>&1 && which unzip > /dev/null 2>&1 || echo "Please install git or wget and unzip" && exit 1
-    wget https://github.com/bauerj/electrumx-installer/archive/master.zip -O /tmp/electrumx-master.zip
-    unzip /tmp/electrumx-master.zip -d ~/.electrumx-installer
-    rm /tmp/electrumx-master.zip
+    wget https://github.com/Groestlcoin/electrumx-grs-installer/archive/master.zip -O /tmp/electrumx-grs-master.zip
+    unzip /tmp/electrumx-grs-master.zip -d ~/.electrumx-grs-installer
+    rm /tmp/electrumx-grs-master.zip
 fi
-cd ~/.electrumx-installer/
+cd ~/.electrumx-grs-installer/
 if [[ $EUID -ne 0 ]]; then
     which sudo > /dev/null 2>&1 || echo "You need to run this script as root" && exit 1
     sudo -H ./install.sh "$@"
